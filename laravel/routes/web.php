6782c00c147421666ce331fd5d0dd16995330cd9
Stream <streamtw.one@gmail.com>
2023-11-02 22:06:23 +0800

    feat: register lfm routes

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'filemanager'], function () {
+    \UniSharp\LaravelFilemanager\Lfm::routes();
+});
